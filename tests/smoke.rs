2767a06aa33343046489fe249eaae055e676d1b1
Without Boats <woboats@gmail.com>
2020-03-12 17:26:16 +0100

    Add more basic smoke tests

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -55,3 +55,47 @@ fn simple_waiting_mut() {
 
     task::block_on(handle);
 }
+
+#[test]
+fn cancel_all_cancels_all() {
+    let map: Arc<WaitMap<String, i32>> = Arc::new(WaitMap::new());
+    let map2 = map.clone();
+
+    let handle = task::spawn(async move {
+        let rosa = map.wait("Rosa Luxemburg");
+        let voltairine = map.wait("Voltairine de Cleyre");
+        assert!(rosa.await.is_none());
+        assert!(voltairine.await.is_none());
+    });
+
+    task::spawn(async move {
+        task::sleep(Duration::from_millis(140)).await;
+        map2.cancel_all();
+    });
+
+    task::block_on(handle);
+}
+
+#[test]
+fn multiple_tasks_can_wait_one_key() {
+    let map: Arc<WaitMap<String, i32>> = Arc::new(WaitMap::new());
+    let map1 = map.clone();
+    let map2 = map.clone();
+
+    task::spawn(async move {
+        map.insert(String::from("Rosa Luxemburg"), 0);
+    });
+
+    let handle1 = task::spawn(async move {
+        let rosa = map1.wait("Rosa Luxemburg").await;
+        assert_eq!(rosa.unwrap().value(), &0);
+    });
+
+    let handle2 = task::spawn(async move {
+        let rosa = map2.wait("Rosa Luxemburg").await;
+        assert_eq!(rosa.unwrap().value(), &0);
+    });
+
+    task::block_on(handle1);
+    task::block_on(handle2);
+}
